fc1b00d5c3dcc8d7fc3d9c8c10a55b827a2d4d8a
Felix Damrau <mail@felixdamrau.de>
2021-10-10 21:44:50 +0200

    Add XML docs
    * include xml and pdb in nuget package
    * add nuget icon

--- src/Core/Result.cs
+++ src/Core/Result.cs
@@ -1,8 +1,21 @@
 ﻿namespace Develix.Essentials.Core;
 
+/// <summary>
+/// This class contains helper methods to create valid and failed results.
+/// </summary>
 public static class Result
 {
+    /// <summary>
+    /// Creates a new instance of a valid <see cref="Result{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the <see cref="Result{T}.Value"/> of the created result.</typeparam>
+    /// <param name="value">The value of the created result.</param>
     public static Result<T> Ok<T>(T value) => new(true, value, default);
 
+    /// <summary>
+    /// Creates a new instance of a failed <see cref="Result{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the <see cref="Result{T}.Value"/> of the created result.</typeparam>
+    /// <param name="message">The message that describes the reason of this failed result.</param>
     public static Result<T> Fail<T>(string message) => new(false, default, message);
 }
