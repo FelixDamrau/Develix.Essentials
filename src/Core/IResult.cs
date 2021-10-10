4d620ccf02cffe4c641de3f3ea459b1f4b3bff0d
Felix Damrau <mail@felixdamrau.de>
2021-10-10 22:30:20 +0200

    Add void result type (#8)
    * Add void result type

---
+++ src/Core/IResult.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Develix.Essentials.Core;
+/// <summary>
+/// Represents the state of an evaluation.
+/// </summary>
+public interface IResult
+{
+    /// <summary>
+    /// The message that describes the state of this <see cref="IResult"/>, for instance, an error message.
+    /// </summary>
+    string? Message { get; }
+
+    /// <summary>
+    /// Specifies whether or not this <see cref="Result{T}"/> represents a successful evaluation.
+    /// </summary>
+    [MemberNotNullWhen(false, nameof(Message))]
+    bool Valid { get; }
+}
--- src/Core/Result.cs
+++ src/Core/Result.cs
@@ -1,10 +1,26 @@
-﻿namespace Develix.Essentials.Core;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Develix.Essentials.Core;
 
 /// <summary>
-/// This class contains helper methods to create valid and failed results.
+/// This class represents a successful or failed evaluation of an action.
+/// It also contains helper methods to create valid and failed results.
 /// </summary>
-public static class Result
+public class Result : IResult
 {
+    /// <inheritdoc />
+    [MemberNotNullWhen(false, nameof(Message))]
+    public bool Valid { get; }
+
+    /// <inheritdoc />
+    public string? Message { get; }
+
+    private Result(bool valid, string? message)
+    {
+        Message = message;
+        Valid = valid;
+    }
+
     /// <summary>
     /// Creates a new instance of a valid <see cref="Result{T}"/>.
     /// </summary>
@@ -13,9 +29,20 @@ public static class Result
     public static Result<T> Ok<T>(T value) => new(true, value, default);
 
     /// <summary>
+    /// Creates a new instance of a valid <see cref="Result"/>.
+    /// </summary>
+    public static Result Ok() => new(true, default);
+
+
+    /// <summary>
     /// Creates a new instance of a failed <see cref="Result{T}"/>.
     /// </summary>
     /// <typeparam name="T">The type of the <see cref="Result{T}.Value"/> of the created result.</typeparam>
     /// <param name="message">The message that describes the reason of this failed result.</param>
     public static Result<T> Fail<T>(string message) => new(false, default, message);
+
+    /// <summary>
+    /// Creates a new instance of a failed <see cref="Result"/>.
+    /// </summary>
+    public static Result Fail(string message) => new(false, message);
 }
